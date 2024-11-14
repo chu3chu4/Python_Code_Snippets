f8b0ce5390aca547848f76687117a96143792912
Chu3Chu4 <hoops.shooter@gmail.com>
2024-11-14 13:43:45 -0600

    weather.py

--- Dictionary.py
+++ Dictionary.py
@@ -7,8 +7,8 @@ Created on Fri Nov  1 22:30:09 2024
 
 cats = {"Jane":16, "Tom":29, "Meow":99}
 
-print (cats["Tom"])
-
+print (cats["Tom"]) #print the value corresponding to key Tom. expected result = 29
+print (cats["Tom"], cats["Meow"]) #print the value corresponding to two keys Tom and Meow. expected result = 29 99
 
 Int_Boolean = {1:True, 2:False, 4:False}
 print (Int_Boolean[4])
--- weather.py
+++ weather.py
@@ -1,10 +1,51 @@
 import requests
 
-# Open Weather API Key 7b76c15c0ef7370b4e74d9638af4a8c9
+# Open Weather API Key cae27cae63ebb705700fc36f91b7db3a
+
+#API_key = "cae27cae63ebb705700fc36f91b7db3a" moved it into Classs Weather Init method
+"""
+#lat and lon of Chicago
+lat = 41.8781
+lon = -87.6298
+
 try:
-    response = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=41.8781&lon=87.6298&exclude={part}&appid=7b76c15c0ef7370b4e74d9638af4a8c9")
+    response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_key}&exclude=minutely,hourly,daily,alerts&units=metric")
     print(response.json())
-#except ConnectionError as e: for reasons I don't understand, this line of code generates a lot of error messages
-except:
+except ConnectionError as e: #for reasons I don't understand, this line of code generates a lot of error messages
     print("you are not connected to network")
 
+temp = response.json()["current"] ["temp"]
+
+print(f"In Chicago, it is currently {temp} °C")
+
+#API call to request weather overview with a human-readable weather summary:
+#human_readable_weather_summary = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/overview?lat={lat}&lon={lon}&appid={API_key}")
+"""
+
+class Weather:
+    def __init__ (self, city, lat, lon, units ="metric"):
+        self.API_key = "cae27cae63ebb705700fc36f91b7db3a"
+        self.city = city
+        self.lat = lat
+        self.lon = lon
+        self.units = units
+        self.fetch_data()
+
+    def fetch_data(self):
+        response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={self.lat}&lon={self.lon}&appid={API_key}&exclude=minutely,hourly,daily,alerts&units=metric")
+        response_json = response.json()
+
+        self.currentTemp = response_json["current"]["temp"]
+        self.currentWind = response_json["current"]["wind_speed"]
+
+    def temp_print(self):
+
+        print(f"it is currently {self.currentTemp} °C in {self.city}")
+        print(f"the speed of the wind in {self.city} is currently {self.currentWind}")
+
+
+Chicago = Weather("Chicago", 41.8781, -87.6298)
+Chicago.temp_print()
+
+Beijing = Weather ("Beijing", 39.916668, 116.383331)
+Beijing.temp_print()
